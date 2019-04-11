a48428dd49468a1ce92bed6cb8b5541fb2e8f6b8
Daria Kravtsova <kravcova.dasha@bk.ru>
2019-04-11 16:32:17 -0600

    Made a list of available chat rooms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomsList from './components/RoomsList'
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDA03YY2qfaeV5bq9RPegkT0GmHO-JB9WQ",
+    authDomain: "rooms-935de.firebaseapp.com",
+    databaseURL: "https://rooms-935de.firebaseio.com",
+    projectId: "rooms-935de",
+    storageBucket: "rooms-935de.appspot.com",
+    messagingSenderId: "578717852404"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Bloc Chat</h1>
+          <RoomsList firebase={firebase} />
         </header>
+        <main>
+        </main>
       </div>
     );
   }
---
+++ src/components/RoomsList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class RoomsList extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+          const room = snapshot.val();
+          room.key = snapshot.key;
+          this.setState({ rooms: this.state.rooms.concat( room ) });
+     });
+  }
+
+  render() {
+    return (
+      <section className="rooms-list">
+        {
+          this.state.rooms.map((room, index) => {
+            return (
+              <div key={room.key}>{room.name}</div>
+            )
+          })
+        }
+      </section>
+    )
+  }
+}
+
+export default RoomsList;
