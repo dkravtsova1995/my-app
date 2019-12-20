12e0e6c4cd7430c05d89474e80c8eaf5ff41fc2a
Daria Kravtsova <kravcova.dasha@bk.ru>
2019-12-19 23:01:33 -0700

    Updates

---
+++ precache-manifest.37988c51f69b1477856ed9022aa5625f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "a75b8cfd30e159b05b9d",
+    "url": "/my-app/static/js/main.9d26aad0.chunk.js"
+  },
+  {
+    "revision": "8fdee894d4dc206d7ba3",
+    "url": "/my-app/static/js/2.4371005a.chunk.js"
+  },
+  {
+    "revision": "a75b8cfd30e159b05b9d",
+    "url": "/my-app/static/css/main.eb6ab138.chunk.css"
+  },
+  {
+    "revision": "67b3b330983a6e8bcdb6762202487d7b",
+    "url": "/my-app/index.html"
+  }
+];
